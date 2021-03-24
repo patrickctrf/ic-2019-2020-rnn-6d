2c0eb4640fe4bb670c7bd94402f9d77519cf2ac8
Patrick de Carvalho Tavares Rezende Ferreira <patrickctrf@gmail.com>
2021-03-24 00:27:42 -0300

    feature: Script with Packed Sequences reached its max performance
    O desempenho usando apenas lstm parece que não irá além da taxa
    de erro atual. Vou retirar as packed sequences e usar batches
    normais para poder usar outras estruturas, como convoluções.

---
+++ src/test_batches_dataset.py
@@ -0,0 +1,3 @@
+from ptk.utils import BatchTimeseriesDataset
+
+BatchTimeseriesDataset()
---
+++ src/verify_dataset_length.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+from matplotlib import pyplot as plt
+from numpy import zeros, load, array, save, where, array_split
+from tqdm import tqdm
+
+from ptk import AsymetricalTimeseriesDataset
+
+dataset = \
+    AsymetricalTimeseriesDataset(
+        x_csv_path="dataset-room2_512_16/mav0/imu0/data.csv",
+        y_csv_path="dataset-room2_512_16/mav0/mocap0/data.csv", shuffle=False,
+        convert_first=False, min_window_size=100, max_window_size=350)
+
+try:
+    tabela = load("tabela_elementos_dataset.npy")
+
+except FileNotFoundError as e:
+    tabela = zeros((len(dataset),))
+    i = 0
+    for element in tqdm(dataset):
+        tabela[i] = element[0].shape[0]
+        i = i + 1
+    save("tabela_elementos_dataset.npy", tabela)
+
+# Estou apenas contando quantas samples eu tenho de cada comprimento
+dict_count = Counter(tabela)
+# So transformei os valores do dict para um array
+ocorrencias = array(list(dict_count.values()))
+
+print(array_split(where(tabela == 167)[0], where(tabela == 167)[0].shape[0] // 64))
+
+plt.plot(tabela)
+plt.show()
+
+x = 1
