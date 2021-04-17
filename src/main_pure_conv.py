46e6a076ef5863b6f4dd8e2fa034da5c0d050035
Patrick de Carvalho Tavares Rezende Ferreira <patrickctrf@gmail.com>
2021-04-16 22:11:34 -0300

    refactor: Trying a smaller network

--- src/main_pure_conv.py
+++ src/main_pure_conv.py
@@ -263,9 +263,9 @@ error within CUDA.
         self.adaptive_pooling = nn.AdaptiveAvgPool1d(pooling_output_size)
 
         self.dense_network = Sequential(
-            nn.Linear(pooling_output_size * n_output_features, 128), nn.LeakyReLU(), nn.BatchNorm1d(128), nn.Dropout(p=0.5),
-            nn.Linear(128, 64), nn.LeakyReLU(),
-            nn.Linear(64, self.output_size)
+            nn.Linear(pooling_output_size * n_output_features, 80), nn.LeakyReLU(), nn.BatchNorm1d(80), nn.Dropout(p=0.5),
+            nn.Linear(80, 40), nn.LeakyReLU(),
+            nn.Linear(40, self.output_size)
         )
         # self.lstm = nn.LSTM(n_output_features, self.hidden_layer_size, batch_first=True, num_layers=self.n_lstm_units, bidirectional=bool(self.bidirectional))
         #
