7cbdc68b752d6c7bf93b640c39cad49853857725
Patrick de Carvalho Tavares Rezende Ferreira <patrickctrf@gmail.com>
2021-06-07 18:34:55 -0300

    checkpoint: Acho que não da pra diminuir mais que isso

--- src/models.py
+++ src/models.py
@@ -133,21 +133,21 @@ error within CUDA.
         n_output_features = 256
         self.feature_extractor = \
             Sequential(
-                Conv1d(input_size, 2 * n_base_filters, (9,), dilation=(2,), stride=(1,)), nn.PReLU(), nn.BatchNorm1d(2 * n_base_filters, affine=True),
+                Conv1d(input_size, 2 * n_base_filters, (7,), dilation=(2,), stride=(1,)), nn.PReLU(), nn.BatchNorm1d(2 * n_base_filters, affine=True),
                 # ResBlock(2 * n_base_filters, 2 * n_base_filters, (7,)),
                 # ResBlock(2 * n_base_filters, 2 * n_base_filters, (9,), dilation=2, stride=1),
-                ResBlock(2 * n_base_filters, n_output_features, (9,), dilation=2, stride=1)
+                ResBlock(2 * n_base_filters, n_output_features, (7,), dilation=2, stride=1)
             )
 
         self.sum_layer = SumLayer(n_output_features)
         self.adaptive_pooling = nn.AdaptiveAvgPool1d(pooling_output_size)
 
         self.dense_network = Sequential(
-            nn.Linear(2 * pooling_output_size * n_output_features, 32), nn.PReLU(), nn.BatchNorm1d(32, affine=True),
+            # nn.Linear(2 * pooling_output_size * n_output_features, 32), nn.PReLU(), nn.BatchNorm1d(32, affine=True),
             # nn.Dropout(p=0.5),
             # nn.Linear(128, 128), nn.PReLU(),
             # # nn.BatchNorm1d(128, affine=True),
-            nn.Linear(32, self.output_size)
+            nn.Linear(2 * pooling_output_size * n_output_features, self.output_size)
         )
         # self.lstm = nn.LSTM(n_output_features, self.hidden_layer_size, batch_first=True, num_layers=self.n_lstm_units, bidirectional=bool(self.bidirectional))
         #
