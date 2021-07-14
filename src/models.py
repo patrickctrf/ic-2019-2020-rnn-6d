2b4b6387050493cfc76a61ecff0f28ae1cb64ed8
Patrick de Carvalho Tavares Rezende Ferreira <patrickctrf@gmail.com>
2021-07-13 22:23:40 -0300

    checkpoint: Desk windows

--- src/models.py
+++ src/models.py
@@ -20,7 +20,7 @@ from ptk.utils.torch import axis_angle_into_rotation_matrix, axis_angle_into_qua
 
 # When importing every models from this module, make sure only models are
 # imported
-__all__ = ["InertialModule", "IMUHandler", "ResBlock", "SumLayer", "IMUHandlerWithPreintegration", "PreintegrationModule"]
+__all__ = ["InertialModule", "IMUHandler", "ResBlock", "SumLayer", "IMUHandlerWithPreintegration", "PreintegrationModule", "EachSamplePreintegrationModule"]
 
 
 class ResBlock(nn.Module):
@@ -99,6 +99,68 @@ class SqueezeAndExcitationBlock1D(nn.Module):
         # We flattened the input, so we resize it back to shape after global
         # pooling and multiply the original input sequence.
         return input_seq * self.squeeze_and_excitation(input_seq).view(-1, self.n_channels, 1)
+
+
+class EachSamplePreintegrationModule(nn.Module):
+    def __init__(self, device=torch.device("cpu"), dtype=torch.float32, imu_freq=200):
+        super().__init__()
+        self.device = device
+        self.dtype = dtype
+
+        self.delta_t = 1 / imu_freq
+
+        self.identity_matrix = torch.eye(n=3, m=3, device=device, dtype=dtype)
+
+    def forward(self, input_seq):
+        """
+    This method computes delta R, v and p (orientation, velocity and position),
+    according to https://arxiv.org/abs/2101.07061 and
+    https://arxiv.org/abs/1512.02363 about inertial feature preintegration.
+
+        :param a_samples: IMU accelerometer input samples to compute over.
+        :param w_samples: IMU gyroscope input samples to compute over.
+        :param initial_velocity: Whenever using a statefull approach, passing
+        initial_velocity will bring up better preintegration results.
+        :return: R (converted from matriz into quaternion), v and p (both 3D tensors).
+        """
+        # orientation matrix
+        delta_r = self.identity_matrix.clone()
+        # velocity tensor (3 element)
+        delta_v = 0.0
+        # position tensor (3 element)
+        delta_p = 0.0
+
+        deltas_p = torch.zeros((input_seq.shape[0], input_seq.shape[1], 3, 1), device=self.device, dtype=self.dtype)
+        deltas_v = torch.zeros((input_seq.shape[0], input_seq.shape[1], 3, 1), device=self.device, dtype=self.dtype)
+        deltas_q = torch.zeros((input_seq.shape[0], input_seq.shape[1], 4), device=self.device, dtype=self.dtype)
+
+        # avoid dividing delta_t by 2 on every loop iteration
+        delta_t_divided_by_2 = self.delta_t / 2
+        square_delta_t_divided_by_2 = delta_t_divided_by_2 * self.delta_t
+
+        w = input_seq.movedim(1, 0)[:, :, :3]
+        a = input_seq.movedim(1, 0)[:, :, 3:].unsqueeze(3)
+        # interactive productory and summation steps
+        for i, (w_k, a_k) in enumerate(list(zip(w, a))):
+            delta_r = torch.matmul(delta_r, axis_angle_into_rotation_matrix(w_k, self.delta_t, device=self.device, dtype=self.dtype))
+            delta_v += torch.matmul(delta_r, a_k * self.delta_t)
+            # Slightly different from original paper, now including
+            # initial_velocity (if available) to compute CURRENT velocity, not
+            # only delta_v (variation)
+            delta_p += delta_v * self.delta_t + torch.matmul(delta_r, a_k * square_delta_t_divided_by_2)
+
+            deltas_p[:, i, :, 0:] = delta_p
+            deltas_v[:, i, :, 0:] = delta_v
+            deltas_q[:, i, :] = \
+                axis_angle_into_quaternion(
+                    *rotation_matrix_into_axis_angle(
+                        delta_r, device=self.device, dtype=self.dtype
+                    ), device=self.device, dtype=self.dtype
+                )
+
+            # noinspection PyTypeChecker
+            return torch.cat((deltas_p.squeeze(3), deltas_q, deltas_v.squeeze(3))
+                             , dim=2)
 
 
 class LSTMLatentFeatures(nn.Module):
@@ -375,11 +437,13 @@ class SingleSamplePreintegrationModule(nn.Module):
         delta_p = delta_v * self.delta_t + torch.matmul(delta_r, input_seq[0, :, 3:] * (self.delta_t ** 2) / 2)
 
         # noinspection PyTypeChecker
-        return torch.cat((delta_p, axis_angle_into_quaternion(*rotation_matrix_into_axis_angle(delta_r, device=self.device, dtype=self.dtype), device=self.device, dtype=self.dtype), delta_v), dim=-1)
+        return torch.cat((delta_p, axis_angle_into_quaternion(*rotation_matrix_into_axis_angle(delta_r, device=self.device, dtype=self.dtype), device=self.device, dtype=self.dtype), delta_v),
+                         dim=-1)
 
 
 class InertialModule(nn.Module):
-    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1, n_lstm_units=1, epochs=150, training_batch_size=64, validation_percent=0.2, bidirectional=False, device=torch.device("cpu"),
+    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1, n_lstm_units=1, epochs=150, training_batch_size=64, validation_percent=0.2, bidirectional=False,
+                 device=torch.device("cpu"),
                  use_amp=True):
         """
 This class implements the classical LSTM with 1 or more cells (stacked LSTM). It
@@ -449,9 +513,9 @@ error within CUDA.
         #                        bidirectional=bidirectional)
 
         self.preintegration_module = \
-            SingleSamplePreintegrationModule(device=self.device)
+            EachSamplePreintegrationModule(device=self.device)
 
-        self.feature_extractor = Conv1DFeatureExtractor(input_size=input_size,
+        self.feature_extractor = Conv1DFeatureExtractor(input_size=10,
                                                         output_size=output_size)
 
         self.dense_network = self.feature_extractor.dense_network
@@ -466,6 +530,8 @@ input sequence and returns the prediction for the final step.
         :param input_seq: Input sequence of the time series.
         :return: The prediction in the end of the series.
         """
+
+        input_seq = self.preintegration_module(input_seq)
 
         # # As features (px, py, pz, qw, qx, qy, qz) sao os "canais" da
         # # convolucao e precisam vir no meio para o pytorch
