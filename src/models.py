c4aabebc14ffd6f3c9a1ce753c0b49c6dc8928a4
patrickctrf <patrickctrf@gmail.com>
2022-01-10 01:27:25 -0300

    checkpoint: Huge dataset.

--- src/models.py
+++ src/models.py
@@ -938,13 +938,13 @@ overflow the memory.
         # train_dataset = Subset(room1_tum_dataset, arange(int(len(room1_tum_dataset) * self.train_percentage)))
         # val_dataset = Subset(room1_tum_dataset, arange(int(len(room1_tum_dataset) * self.train_percentage), len(room1_tum_dataset)))
 
-        # train_dataset = ConcatDataset([euroc_v1_01_dataset, euroc_v1_02_dataset,
-        #                                euroc_mh1_dataset, euroc_mh5_dataset,
-        #                                euroc_v2_03_dataset, euroc_mh4_dataset])
-        # val_dataset = ConcatDataset([euroc_v2_02_dataset, euroc_mh3_dataset])
+        train_dataset = ConcatDataset([euroc_v1_01_dataset, euroc_v1_02_dataset,
+                                       euroc_mh1_dataset, euroc_mh5_dataset,
+                                       euroc_v2_03_dataset, euroc_mh4_dataset])
+        val_dataset = ConcatDataset([euroc_v2_02_dataset, euroc_mh3_dataset])
 
-        train_dataset = ConcatDataset([euroc_v1_01_dataset, ])
-        val_dataset = ConcatDataset([euroc_mh3_dataset, ])
+        # train_dataset = ConcatDataset([euroc_v1_01_dataset, ])
+        # val_dataset = ConcatDataset([euroc_mh3_dataset, ])
 
         train_loader = CustomDataLoader(dataset=train_dataset, batch_size=1, shuffle=True, pin_memory=True, num_workers=4, multiprocessing_context='spawn')
         val_loader = CustomDataLoader(dataset=val_dataset, batch_size=1, shuffle=True, pin_memory=True, num_workers=4, multiprocessing_context='spawn')
