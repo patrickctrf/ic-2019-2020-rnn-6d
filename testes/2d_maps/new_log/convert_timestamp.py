9b7a59190eeea988f082618eaee649230e6343cf
patrickctrf <patrickctrf@gmail.com>
2022-07-06 23:12:43 -0300

    checkpoint: Tests over

---
+++ testes/2d_maps/new_log/convert_timestamp.py
@@ -0,0 +1,16 @@
+# Load the Pandas libraries with alias 'pd'
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+
+# precisa ter uma dimensao de batch e uma dimensao de CANAL, que nem convolucao.
+df = pd.read_csv("gt_mh01.csv", sep=" ", float_precision='round_trip').to_numpy()
+
+df[:, 0] = df[:, 0] / 1e9
+
+df = pd.DataFrame(df)
+
+df.to_csv("gt_mh01.csv", index=False, header=False, sep=" ", float_format='%.9f')
+
+print(df.head())
